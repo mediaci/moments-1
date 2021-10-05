8c243897bf652e18096a16452fd9f86e17512af4
A Greaves <anna.j.greaves@gmail.com>
2021-10-05 14:22:16 +0000

    10 - refreshing access tokens

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
