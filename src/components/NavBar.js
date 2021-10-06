030d00ea4ef123337f7edcfcbf0e387c4dcc2381
A Greaves <anna.j.greaves@gmail.com>
2021-10-06 11:27:06 +0000

    12 - NavBar burger toggle with a custom hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,12 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -19,7 +21,7 @@ const NavBar = () => {
       console.log(err);
     }
   };
-  
+
   const loggedInIcons = (
     <>
       <NavLink
@@ -83,7 +85,7 @@ const NavBar = () => {
   )
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top" expanded={expanded}>
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -91,7 +93,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle 
+          aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(!expanded)}
+          ref={ref}
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
