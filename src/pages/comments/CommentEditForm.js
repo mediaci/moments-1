f1fcb6d163fa4abbe147605a5eee7a3b3c31897b
A Greaves <anna.j.greaves@gmail.com>
2021-10-28 11:22:32 +0000

    Add sourcecode for 19d.

---
+++ src/pages/comments/CommentEditForm.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+
+import styles from "../../styles/CommentCreateEditForm.module.css";
+
+function CommentEditForm(props) {
+  const {
+    id,
+    profile_id,
+    content,
+    setShowEditForm,
+    setComments,
+    profileImage,
+  } = props;
+
+  const [formContent, setFormContent] = useState(content);
+
+  const handleChange = (event) => {
+    setFormContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put(`/comments/${id}/`, {
+        content: formContent.trim(),
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? { ...comment, content: formContent.trim() }
+            : comment;
+        }),
+      }));
+      setShowEditForm(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <InputGroup>
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profileImage} />
+          </Link>
+          <Form.Control
+            className={styles.Form}
+            as="textarea"
+            value={formContent}
+            onChange={handleChange}
+            rows={4}
+          />
+        </InputGroup>
+      </Form.Group>
+      <div className="text-right">
+        <button
+          className={styles.Button}
+          disabled={!content.trim()}
+          type="submit"
+        >
+          save
+        </button>
+        <button
+          className={styles.Button}
+          onClick={() => setShowEditForm(false)}
+        >
+          cancel
+        </button>
+      </div>
+    </Form>
+  );
+}
+
+export default CommentEditForm;
