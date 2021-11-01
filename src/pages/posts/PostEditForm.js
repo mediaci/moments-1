d05a857e04308e584763771385d30d0e039a07ac
A Greaves <anna.j.greaves@gmail.com>
2021-11-01 08:40:27 +0000

    Remove unneeded imports

--- src/pages/posts/PostEditForm.js
+++ src/pages/posts/PostEditForm.js
@@ -8,10 +8,6 @@ import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
 import Image from "react-bootstrap/Image";
 
-import Asset from "../../components/Asset";
-
-import Upload from "../../assets/upload.png";
-
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
