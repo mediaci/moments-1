eb4e4bb269548d17215bb8d28f8df1a1b21092ad
A Greaves <anna.j.greaves@gmail.com>
2021-11-25 14:30:29 +0000

    Add code for 29a

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -2,78 +2,83 @@ import { createContext, useEffect, useState, useContext, useMemo } from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useHistory } from "react-router";
+import { removeTokenTimestamp, shouldRefreshToken } from "../utils/utils";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
 
 export const CurrentUserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const history = useHistory();
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
 
-    const handleMount = async () => {
-        try {
-            const { data } = await axiosRes.get("dj-rest-auth/user/");
-            setCurrentUser(data);
-        } catch (err) {
-            console.log(err);
+  const handleMount = async () => {
+    try {
+      const { data } = await axiosRes.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
+  useMemo(() => {
+    axiosReq.interceptors.request.use(
+      async (config) => {
+        if (shouldRefreshToken()) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+            console.log("refreshed the token OK!", config);
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push("/signin");
+              }
+              return null;
+            });
+            removeTokenTimestamp();
+            return config;
+          }
         }
-    };
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
 
-    useEffect(() => {
-        handleMount();
-    }, []);
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push("/signin");
+              }
+              return null;
+            });
+            removeTokenTimestamp();
+          }
+          return axios(err.config);
+        }
+        return Promise.reject(err);
+      }
+    );
+  }, [history]);
 
-    useMemo(() => {
-        axiosReq.interceptors.request.use(
-            async (config) => {
-                try {
-                    await axios.post("/dj-rest-auth/token/refresh/");
-                } catch (err) {
-                    setCurrentUser((prevCurrentUser) => {
-                        if (prevCurrentUser) {
-                            history.push("/signin");
-                        }
-                        return null;
-                    });
-                    return config;
-                }
-                return config;
-            },
-            (err) => {
-                return Promise.reject(err);
-            }
-        )
-
-        axiosRes.interceptors.response.use(
-            (response) => response,
-            async (err) => {
-                if (err.response?.status === 401) {
-                    try {
-                        await axios.post("/dj-rest-auth/token/refresh/");
-                    } catch (err) {
-                        setCurrentUser((prevCurrentUser) => {
-                            if (prevCurrentUser) {
-                                history.push("/signin");
-                            }
-                            return null;
-                        });
-                    }
-                    return axios(err.config);
-                }
-                return Promise.reject(err);
-            }
-        );
-    }, [history]);
-
-    return (
-        <CurrentUserContext.Provider value={currentUser}>
-            <SetCurrentUserContext.Provider value={setCurrentUser}>
-                {children}
-            </SetCurrentUserContext.Provider>
-        </CurrentUserContext.Provider>
-
-    )
-}
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        {children}
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
+  );
+};
 
 export const useCurrentUser = () => useContext(CurrentUserContext);
 export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,54 +1,68 @@
-import { axiosReq } from "../api/axiosDefaults"
+import jwtDecode from "jwt-decode";
+import { axiosReq } from "../api/axiosDefaults";
 
 export const fetchMoreData = async (resource, setResource) => {
-    try {
-        const {data} = await axiosReq.get(resource.next);
-        setResource((prevResource) => ({
-            ...prevResource,
-            next: data.next,
-            results: data.results.reduce((acc, cur) => {
-                return acc.some((accResult) => accResult.id === cur.id) ? acc : [...acc, cur];
-            },
-            prevResource.results),
-          }));
-      
-    } catch (err) {
-        console.log(err)
-    }
-}
+  try {
+    const { data } = await axiosReq.get(resource.next);
+    setResource((prevResource) => ({
+      ...prevResource,
+      next: data.next,
+      results: data.results.reduce((acc, cur) => {
+        return acc.some((accResult) => accResult.id === cur.id)
+          ? acc
+          : [...acc, cur];
+      }, prevResource.results),
+    }));
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const followHelper = (profile, clickedProfile, following_id) => {
-    return profile.id === clickedProfile.id
-      ? // This is the profile I clicked on, update its followers
-        // count and set its following id
-        {
-          ...profile,
-          followers_count: profile.followers_count + 1,
-          following_id,
-        }
-      : profile.is_owner
-      ? // This is the profile of the logged in user
-        // updated its following count
-        { ...profile, following_count: profile.following_count + 1 }
-      : // this is neither the profile we clicked on the profile we own
-        // so just return it unchanged
-        profile;
-  }
+  return profile.id === clickedProfile.id
+    ? // This is the profile I clicked on, update its followers
+      // count and set its following id
+      {
+        ...profile,
+        followers_count: profile.followers_count + 1,
+        following_id,
+      }
+    : profile.is_owner
+    ? // This is the profile of the logged in user
+      // updated its following count
+      { ...profile, following_count: profile.following_count + 1 }
+    : // this is neither the profile we clicked on the profile we own
+      // so just return it unchanged
+      profile;
+};
 
-  export const unfollowHelper = (profile, clickedProfile) => {
-    return profile.id === clickedProfile.id
-      ? // This is the profile I clicked on, update its followers
-        // count and set its following id
-        {
-          ...profile,
-          followers_count: profile.followers_count - 1,
-          following_id: null,
-        }
-      : profile.is_owner
-      ? // This is the profile of the logged in user
-        // updated its following count
-        { ...profile, following_count: profile.following_count - 1 }
-      : // this is neither the profile we clicked on the profile we own
-        // so just return it unchanged
-        profile;
-  }
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? // This is the profile I clicked on, update its followers
+      // count and set its following id
+      {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? // This is the profile of the logged in user
+      // updated its following count
+      { ...profile, following_count: profile.following_count - 1 }
+    : // this is neither the profile we clicked on the profile we own
+      // so just return it unchanged
+      profile;
+};
+
+export const setTokenTimestamp = (data) => {
+  const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+  localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
+};
+
+export const shouldRefreshToken = () => {
+  return !!localStorage.getItem("refreshTokenTimestamp");
+};
+
+export const removeTokenTimestamp = () => {
+  localStorage.removeItem("refreshTokenTimestamp");
+};
